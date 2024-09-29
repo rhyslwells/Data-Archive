dbc562a00afdbc80368c87a164963aa1c2d34ebf
Rhys Wells <rhyslwells@outlook.com>
2024-09-29 09:04:46 +0100

    finally?

--- .history/quartz/components/LinksHeader_20240929090244.tsx
+++ .history/quartz/components/LinksHeader_20240929090244.tsx
@@ -17,7 +17,7 @@ export default (() => {
             <a href="Data-Archive/pages/Questions">Questions</a> 
           </span>
           <span>
-          <a href="/Data-Archive/Tasks">Tasks</a>
+          <a href="/Tasks">Tasks</a>
           </span>
         </div>
       <hr style="background-color: var(--gray); border-top: 1px var(--gray) solid; margin-top: 1.3rem"></hr>
