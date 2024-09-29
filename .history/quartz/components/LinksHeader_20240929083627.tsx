afd81e672314269bec86641309778319ebc5bf46
Rhys Wells <rhyslwells@outlook.com>
2024-09-29 08:41:31 +0100

    pages

--- .history/quartz/components/LinksHeader_20240929083627.tsx
+++ .history/quartz/components/LinksHeader_20240929083627.tsx
@@ -11,13 +11,13 @@ export default (() => {
       <div>
         <div id="links-header">
           <span>
-            <a href="./pages/Data_Archive">Data Archive</a>
+            <a href="*/pages/Data_Archive">Data Archive</a>
           </span>
           <span>
-            <a href="./pages/Questions">Questions</a> 
+            <a href="//pages/Questions">Questions</a> 
           </span>
           <span>
-          <a href="./Tasks">Tasks</a>
+          <a href="/Tasks">Tasks</a>
           </span>
         </div>
       <hr style="background-color: var(--gray); border-top: 1px var(--gray) solid; margin-top: 1.3rem"></hr>
--- .history/quartz/components/LinksHeader_20240929084012.tsx
+++ .history/quartz/components/LinksHeader_20240929084012.tsx
@@ -17,7 +17,7 @@ export default (() => {
             <a href="./pages/Questions">Questions</a> 
           </span>
           <span>
-          <a href="./Tasks">Tasks</a>
+          <a href="/Tasks">Tasks</a>
           </span>
         </div>
       <hr style="background-color: var(--gray); border-top: 1px var(--gray) solid; margin-top: 1.3rem"></hr>
